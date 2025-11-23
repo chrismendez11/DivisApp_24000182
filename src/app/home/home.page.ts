01bf4eff81551467fdf5bde4292196b67ad2a124
Christian Mendez <christianmndz.dev@gmail.com>
2025-11-23 15:45:54 -0600

    Add currency converter functionality - Quetzales to USD

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,18 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  cantidadQuetzales: number = 0;
+  cantidadDolares: string = '0.00';
+  tasaCambio: number = 8;
 
   constructor() {}
 
+  convertir() {
+    if (this.cantidadQuetzales && this.cantidadQuetzales > 0) {
+      const resultado = this.cantidadQuetzales / this.tasaCambio;
+      this.cantidadDolares = resultado.toFixed(2);
+    } else {
+      this.cantidadDolares = '0.00';
+    }
+  }
 }
